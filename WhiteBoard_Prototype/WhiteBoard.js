a7ab1a02b72cf6e93074ee0a381d80f50a24b913
Leoncio-Kian <leoncio.kian@ufl.edu>
2016-02-05 10:35:32 -0500

    updated the whiteboard

--- WhiteBoard_Prototype/WhiteBoard.js
+++ WhiteBoard_Prototype/WhiteBoard.js
@@ -2,14 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var whiteboardArray = [];
+
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/WhiteBoard.html');
 });
 
 io.on('connection', function(socket){
 	console.log('a user has connected');
+	console.log(socket.id);
+	io.emit('initializeWhiteboard', whiteboardArray);
+
 	socket.on('whiteboard update', function(msg){
-		console.log('message: ' + msg.plots[0].x + msg.plots[0].y);
+		whiteboardArray[socket.id] = msg.plotArray;
+		//console.log('message: ' + msg.plots.startLoc + msg.plots.endLoc);
 		io.emit('whiteboard update', msg);
 	});
 	socket.on('disconnect', function(){
