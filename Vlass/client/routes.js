fd2f03cc09837f84d9bb05cf724e3f30637c8efb
Leoncio-Kian <leoncio.kian@ufl.edu>
2016-02-22 22:41:58 -0500

    added meteor version of vlass

---
+++ Vlass/client/routes.js
@@ -0,0 +1,20 @@
+Router.configure({
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading'
+});
+
+Router.route('/', {name: 'home', controller: 'MainController'});
+
+MainController = RouteController.extend({
+  action: function() {
+  	this.render('home', {
+	    data: function () {
+	      return { posts: ['post red', 'post blue'] }
+	    }
+  	});
+  }
+});
+
+Router.route('/whiteboard', function () {
+	this.render('whiteboard');
+});
